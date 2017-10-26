7a5a49a755dcc14e3faeb1f1551a2d09ebcb1e63
Andreas Nilsen <adde88@gmail.com>
2017-10-26 13:50:42 +0200

    Add UnitChecks to fix Overlays
    This will fix Overlays with latest FH b40

--- player.lua
+++ player.lua
@@ -2,15 +2,15 @@ local _, gbl = ...
 
 function gbl:PlayerSync()
 	-- Melee range
-	if self:F('p_MELEE') then
+	if self:F('p_MELEE') and _G.UnitExists('target') and _G.ObjectIsVisible('target') then
 		self:Circle('player', self:CombatReach('player', 'target') + 1.5)
 	end
 	-- Melee range
-	if self:F('p_RANGED') then
+	if self:F('p_RANGED') and _G.UnitExists('target') and _G.ObjectIsVisible('target') then
 		self:Circle('player', self:CombatReach('player', 'target') + 40)
 	end
 	-- Targets
-	if self:F('p_TLINES') then
+	if self:F('p_TLINES') and _G.UnitExists('target') and _G.ObjectIsVisible('target') then
 			self:DrawLine('player', 'target')
 	end
 end
