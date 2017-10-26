927fe85e7bb3a1e925037f21dd11612929f1ced1
Andreas Nilsen <adde88@gmail.com>
2017-10-26 22:33:13 +0200

    Fixes some errors when trying to load Overlays.

--- target.lua
+++ target.lua
@@ -2,15 +2,15 @@ local _, gbl = ...
 
 function gbl:TargetSync()
 	-- Melee range
-	if self:F('t_MELEE') then
+	if self:F('t_MELEE') and _G.UnitExists('target') and _G.ObjectIsVisible('target') then
 		self:Circle('target', self:CombatReach('player', 'target') + 1.5)
 	end
 	-- Melee range
-	if self:F('t_RANGED') then
+	if self:F('t_RANGED') and _G.UnitExists('target') and _G.ObjectIsVisible('target') then
 		self:Circle('target', self:CombatReach('player', 'target') + 40)
 	end
 	-- Targets
-	if self:F('t_TLINES') and _G.ObjectIsVisible('targettarget') then
+	if self:F('t_TLINES') and _G.UnitExists('target') and _G.ObjectIsVisible('targettarget') then
 			self:DrawLine('target', 'targettarget')
 	end
 end
