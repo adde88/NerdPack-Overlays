c9661f250b1c2042ac4aaf8a2a2b1083337a18c1
Andreas Nilsen <adde88@gmail.com>
2017-10-30 09:49:41 +0100

    Check units to fix error message

--- player.lua
+++ player.lua
@@ -2,15 +2,15 @@ local _, gbl = ...
 
 function gbl:PlayerSync()
 	-- Melee range
-	if self:F('p_MELEE') then
+	if self:F('p_MELEE') and NeP.Protected.ObjectExists('target') then
 		self:Circle('player', self:CombatReach('player', 'target') + 1.5)
 	end
 	-- Melee range
-	if self:F('p_RANGED') then
+	if self:F('p_RANGED') and NeP.Protected.ObjectExists('target') and _G.ObjectIsVisible('target') then
 		self:Circle('player', self:CombatReach('player', 'target') + 40)
 	end
 	-- Targets
-	if self:F('p_TLINES') then
+	if self:F('p_TLINES') and NeP.Protected.ObjectExists('target') and _G.ObjectIsVisible('target') then
 			self:DrawLine('player', 'target')
 	end
 end
